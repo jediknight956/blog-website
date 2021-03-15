43d09da9814c2a0dd94e6aa8e936fcfa9b447e7a
abartz96 <43457105+abartz96@users.noreply.github.com>
2021-03-14 20:25:59 -0500

    added the homepage

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,7 +13,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
 
 
