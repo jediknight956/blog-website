19cde32fb30044b0eea6342fd50ecff08837a7fc
abartz96 <43457105+abartz96@users.noreply.github.com>
2021-03-14 20:54:08 -0500

    added About and Contact pages

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home");
+  res.render("home", {content: homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {content: aboutContent});
+});
 
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {content: contactContent});
+});
 
 
 
