d80d6b7754db9e8dc623e10c4de34deed7be3a14
abartz96 <43457105+abartz96@users.noreply.github.com>
2022-08-07 18:24:30 -0500

    display blog posts on homepage, blog post urls

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+const port = 3000;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,6 +13,7 @@ const app = express();
 //Globals
 let posts = [];
 
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -38,17 +41,29 @@ app.get("/compose", function(req, res){
 app.post("/compose", function(req, res){
   const post = {
     title: req.body.title,
-    content: req.body.composition
+    content: req.body.composition,
+    link: "posts/" + _.kebabCase(req.body.title)
   };
   posts.push(post);
   res.redirect("/");
 });
 
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+  
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title)
 
+    if (storedTitle === requestedTitle){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    } 
+  });
+  
+});
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started at localhost:"+port);
 });
